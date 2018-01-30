d3bc86c76b9914e8b6b9ae043b9d13cb1dd5c2e7
hmmChase <johndoe@example.com>
2018-01-29 18:46:46 -0700

    Add spaceship alive test, remove firebullet function, change canvas size

--- test/Spaceship-test.js
+++ test/Spaceship-test.js
@@ -1,12 +1,16 @@
 const chai = require('chai');
-const { assert } = chai;
-// const assert = chai.assert;
+const { assert, expect } = chai;
 const Spaceship = require('../lib/Spaceship.js')
 
 describe('Spaceship', function() {
 
   it('should be a function', function () {
     assert.isFunction(Spaceship)
+  });
+
+  it('spaceship is alive', function () {
+    let spaceship = new Spaceship();
+    assert.equal(spaceship.alive, true)
   });
 
   
