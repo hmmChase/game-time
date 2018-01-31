6ebf6da6167808166def216b8cbe01bd0acf8626
Matt Walker <mttwlkr@gmail.com>
2018-01-30 20:52:07 -0700

    Add 25 tests, add canvas testing ability

--- test/Bullet-test.js
+++ test/Bullet-test.js
@@ -1,18 +1,25 @@
 const chai = require('chai');
 const { assert, expect } = chai;
-// const assert = chai.assert;
 const Bullet = require('../lib/Bullet.js')
 
 describe('Bullet', function() {
+  let bullets = [];
+  
+  beforeEach(function () {
+    for (let i = 0; i < 1; i++) {
+    bullets.push(new Bullet());
+    }
+  })
 
   it('should be a function', function () {
     assert.isFunction(Bullet)
   });
 
   it('should be a small bullet', function () {
-    const bullet = new Bullet();
-    assert.equal(bullet.radius, 3)
+    assert.equal(bullets[0].radius, 3)
   })
+
+  
 
   
 })
